263fba5f7edfbb1a9f0bb85fc2e08bda59b243e8
Martialis39 <sass@velvet.ee>
2020-03-09 19:52:39 +0200

    Add a basic test

--- test/main.test.js
+++ test/main.test.js
@@ -1,3 +1,17 @@
 const chai = require("chai")
 const sinon = require("sinon")
 const expect = chai.expect;
+
+const checkBoard = x => x;
+
+describe("Check board function", function(){
+  describe("Empty board", function(){
+    it("should return an empty array", function(){
+      const emptyBoard = [];
+      const result = checkBoard(emptyBoard);
+      expect(result).to.deep.equal([]);
+    })
+  })
+})
+
+
